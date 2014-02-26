579f568d4340f5b750c32ca0568ba02246360330
David Moles <chronodm@gmail.com>
2014-02-26 13:31:33 -0800

    Added comments and logging

--- src/main/scala/apsu/core/Entity.scala
+++ src/main/scala/apsu/core/Entity.scala
@@ -4,19 +4,38 @@ import com.fasterxml.uuid.{Generators, EthernetAddress}
 import java.util.UUID
 import org.apache.log4j.Logger
 
+/**
+ * The fundamental building block of the entity system.
+ * @param id The UUID of the entity. Two entities with the same UUID are considered identical.
+ */
 case class Entity(id: UUID)
 
+/**
+ * Factory class for constructing entities.
+ */
 object Entity {
+
+  // ------------------------------------------------------------
+  // Private fields
 
   private val log = Logger.getLogger(classOf[Entity])
 
   private val uuidGen = Generators.timeBasedGenerator({
-    Option(EthernetAddress.fromInterface()) match {
+    val enetAddr = Option(EthernetAddress.fromInterface()) match {
       case Some(addr) => addr
       case _ => EthernetAddress.constructMulticastAddress()
     }
+    log.info(s"Initializing UUID generator with Ethernet address ${enetAddr.toString}")
+    enetAddr
   })
 
+  // ------------------------------------------------------------
+  // Public methods
+
+  /**
+   * Constructs a new entity with a globally unique ID.
+   * @return A new entity
+   */
   def apply(): Entity = {
     Entity(uuidGen.generate())
   }
