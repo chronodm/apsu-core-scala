65e1516c06108dc3d4d6546f0dcac883f5bf6839
David Moles <chronodm@gmail.com>
2014-02-27 17:12:18 -0800

    Added TODO and comments

--- src/main/scala/apsu/core/System.scala
+++ src/main/scala/apsu/core/System.scala
@@ -3,3 +3,24 @@ package apsu.core
 trait System {
   def update(e: Entity)
 }
+
+/*
+ * TODO: Rewrite entity/system/component relationship
+ *
+ * System should be a function, not a class.
+ *
+ * Submitting a system to... something should efficiently find all entities
+ * with the necessary components for that system, and run it against them.
+ *
+ * The state of all entity-component relationships should be explicitly modeled
+ * as a monad (whatever the hell that would mean).
+ *
+ * "Side" effects of systems should be expressed in terms of deltas, not final
+ * states, and deltas should be queued up, merged, and applied at the end of
+ * the tick, in some fantastically efficient manner.
+ *
+ * That stipulation might not actually make any sense if not every system is
+ * on the same clock. If not every system is on the same clock, transactionality
+ * needs rethinking.
+ */
+
