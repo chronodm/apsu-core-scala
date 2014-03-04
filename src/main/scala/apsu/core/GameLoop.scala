27ac26f3433c49bee90bda6a7e6456902e0f259a
David Moles <chronodm@gmail.com>
2014-03-04 09:00:00 -0800

    Added todo

--- src/main/scala/apsu/core/GameLoop.scala
+++ src/main/scala/apsu/core/GameLoop.scala
@@ -4,6 +4,7 @@ import apsu.core.world.{Update, World}
 
 trait GameLoop extends System {
 
+  // TODO implement system preconditions/postconditions & sorting, cf. http://t-machine.org/index.php/2009/10/26/entity-systems-are-the-future-of-mmos-part-5/comment-page-3/#comment-97097
   def systems: Seq[System]
 
   override def invoke(w: World): Seq[Update] = {
